6f89efd9d459bb622712ed533c5bb255f0444a1a
b-regan11 <reganbill02@gmail.com>
2024-05-19 12:29:55 -0400

    Header and Image Slider

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +24,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+} */
+body {
+  background-color: lavenderblush;
 }
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Here is the Your Vue.js App"/>
+    <h1>Jenill's 21st Birthday</h1>
+    <div class="slider">
+      <div class="slider-wrapper" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+        <div v-for="(image, index) in images" :key="index" class="slide">
+          <img :src="image" :alt="'Slide ' + index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      currentIndex: 0,
+      images: [
+        require('@/assets/Jenill1.jpeg'),
+        require('@/assets/Jenill2.jpeg'),
+        require('@/assets/Jenill3.jpeg'),
+        require('@/assets/Jenill4.jpeg'),
+        require('@/assets/Jenill5.jpeg'),
+        require('@/assets/Jenill6.jpeg'),
+        require('@/assets/Jenill7.png'),
+      ]
+    };
+  },
+  mounted() {
+    this.startSlider();
+    this.setRandomStart();
+    this.setTitle();
+  },
+  methods: {
+    startSlider() {
+      setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      }, 10000); // Change slide every 10 seconds
+    },
+    setRandomStart() {
+      this.currentIndex = Math.floor(Math.random() * this.images.length);
+    },
+    setTitle() {
+      document.title = "Jenill's Birthday";
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.slider {
+  overflow: hidden;
+  width: 277px;
+  height: 350px;
+  position: relative;
+}
+
+.slider-wrapper {
+  display: flex;
+  transition: transform 0.5s ease;
+}
+
+.slide {
+  min-width: 100%;
+  height: 100%;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.home {
+  background-color: lavenderblush;
+}
+
+</style>

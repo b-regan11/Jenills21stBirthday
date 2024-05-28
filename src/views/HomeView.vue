d2d8f718a8d060b5e1c4a5cf17a159779370ea43
b-regan11 <reganbill02@gmail.com>
2024-05-28 01:47:20 -0400

    Event Info

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1 class="TopHeader">Jenill's 21st Birthday</h1>
-    <!-- <h2 class="TopText">Can You Believe It?</h2> -->
+    <p class="TopText">Can You Believe It?</p>
+    <p class="MiddleText">Please Join Us For A Birthday Celebration</p>
+    <p class="BottomText">If Gifting, Contributions To The College Fund Greatly Appreciated</p>
 
       <div class="slider">
         <div class="slider-wrapper" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
@@ -10,7 +12,12 @@
           </div>
         </div>
       </div>
-
+      <div class="eventInfo">
+        <p>July 14th | 4:00 pm</p>
+        <p>East End Yacht Club</p>
+        <p><a href="https://www.google.com/maps/search/?api=1&query=104+Bay+Street,+Bridgeport,+CT" target="_blank">104 Bay Street, Bridgeport, CT</a></p>
+        <p>RSVP w/ Will Perez Jr by July 1st</p>
+      </div>
 
   </div>
 </template>
@@ -26,8 +33,6 @@ export default {
         require('@/assets/Jenill3.jpeg'),
         require('@/assets/Jenill4.jpeg'),
         require('@/assets/Jenill5.jpeg'),
-        require('@/assets/Jenill6.jpeg'),
-        require('@/assets/Jenill7.png'),
       ]
     };
   },
@@ -66,33 +71,74 @@ export default {
   align-items: center;
   text-align: center;
   grid-row: 1;
-  
+  font-size: 75px;
   font-family: "Brush Script MT", "Lucida Handwriting", cursive;
-  font-size: 60px;
   color:palevioletred;
   text-shadow: 
-    -1px -1px 0 white,
-    1px -1px 0 white,
+    -1px -1px 0 black,
+    1px -1px 0 rgb(141, 132, 84),
     -1px 1px 0 white,
     1px 1px 0 white;
-
+  margin-bottom: 10px;
 }
 
-.TopText {
-  display: flex;
+.eventInfo {
+  grid-row: 6;
+}
+
+.eventInfo p {
+    font-family: 'Open Sans', Arial, sans-serif;
+    font-size: 18px;
+    color: #333;
+    margin: 0 0;
+    margin-top: 1px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 24px;
+  }
+
+.eventInfo a {
+  text-decoration: none;
+  color: #333;
+  margin: 0 0;
+  margin-top: 1px;
   justify-content: center;
   align-items: center;
   text-align: center;
+  font-size: 24px;
+}
+
+.TopText {
+  margin: 0 0;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 22px;
   grid-row: 2;
-  
-  /* font-family: "Brush Script MT", "Lucida Handwriting", cursive;
-  font-size: 60px;
-  color:palevioletred;
-  text-shadow: 
-    -1px -1px 0 white,
-    1px -1px 0 white,
-    -1px 1px 0 white,
-    1px 1px 0 white; */
+  font-family: 'Open Sans', Arial, sans-serif;
+  color: #333;
+}
+.MiddleText {
+  margin: 0 0;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 20px;
+  grid-row: 3;
+  font-family: 'Open Sans', Arial, sans-serif;
+  color: #333;
+}
+.BottomText {
+  margin: 0 0;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  grid-row: 4;
+  font-size: 20px;
+  font-family: 'Open Sans', Arial, sans-serif;
+  color: #333;
+  margin-bottom: 10px;
 }
 
 .slider {
@@ -102,9 +148,10 @@ export default {
   position: relative;
   
   display: flex; 
+  justify-self: center;
   justify-content: center; 
   align-items: center;
-  grid-row: 3;
+  grid-row: 5;
 }
 
 .slider-wrapper {
@@ -125,10 +172,53 @@ img {
 
 @media (max-width: 450px) {
   .slider {
-    width: 80%;
+    width: 70%;
     max-width: none;
     height: auto;
     margin: 0 auto;
+    margin-bottom: 5px;
+  }
+  .TopHeader {
+    margin: 0 0;
+    margin-top: 10px;
+    font-size: 60px;
+  }
+  .TopText {
+    margin: 0 0;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 20px;
+  }
+  .MiddleText {
+    margin: 0 0;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 16px;
+  }
+  .BottomText {
+    margin: 0 0;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+  .eventInfo p {
+    margin: 0 0;
+    margin-top: 2px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 20px;
+  }
+  .eventInfo a {
+    margin: 0 0;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 20px;
   }
 }
 
